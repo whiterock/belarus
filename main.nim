dadcdaf7a7fbec12dc9f14503a57cf4174c2c05c
whiterock <whiterockftp@gmail.com>
2020-05-25 15:38:40 +0200

    refactored, added debugging output, implemented registers, etc.

--- main.nim
+++ main.nim
@@ -6,8 +6,8 @@ type
     siList,      
   StackItem = ref object
     case kind: StackItemKind # the ``kind`` field is the discriminator
-    of siNumber: numberVal: float
-    of siList: listVal: seq[char]
+    of siNumber: numberVal: float # 0.0 by default
+    of siList: listVal: seq[char] # empty by default
 
 # Implements echo/repr for our StackItem
 proc `$`(si: StackItem): string =
@@ -27,84 +27,102 @@ var stack = initDeque[StackItem]()
 #stack.addLast(StackItem(kind: siList, listVal: @[9.1, 9.2, 9.3]))
 
 #var commandStream = "4 2-" # TODO: Initialize this from register a
-var commandStream = newStringStream("423.15")
+let command = "423.15(3.14)C2.718+c"
+var commandStream = newStringStream(command)
 var operationMode: int = 0
-
+var register: array['a'..'z', StackItem] # yep crazy shit like that is part of nim-lang
 
 #############
 # Main loop #
 #############
-while true:
-
-  var ch = readChar(commandStream)
-
-  if ch == '\0':
-    break
-
+while (var ch = readChar(commandStream); ch) != '\0':
+  echo command
+  echo spaces(commandStream.getPosition()-1) & "\e[1;34m^\e[00m"
   case operationMode:
   # Decimal Place Construction Mode
+  # REMARK: Fully implemented
   of low(int)..(-2):
     case ch:
     of '0'..'9':
-      var top = popLast(stack)
-      var a = top.numberVal + float((int(ch) - int('0'))) * pow(10, float(operationMode+1))
+      var top = stack.popLast()
 
-      top.numberVal = a
-      addLast(stack, top)
+      top.numberVal += parseFloat($ch) * pow(10, float(operationMode+1))
+      stack.addLast(top)
       operationMode -= 1
-
     of '.':
-      addLast(stack, StackItem(kind: siNumber, numberVal: float(0)))
+      stack.addLast(StackItem(kind: siNumber))
       operationMode = -2
     else:
       operationMode = 0
-      setPosition(commandStream, getPosition(commandStream)-1)
+      commandStream.setPosition(getPosition(commandStream)-1)
 
   # Whole Number Construction Mode
+  # REMARK: Fully implemented
   of -1:
     case ch:
     of '0'..'9':
-      var top = popLast(stack)
+      var top = stack.popLast()
 
-      var a = top.numberVal * 10 + float((int(ch) - int('0')))
-      top.numberVal = a
-      addLast(stack, top)
-
+      top.numberVal = top.numberVal * 10 + parseFloat($ch)
+      stack.addLast(top)
     of '.':
       operationMode = -2
     else:
       operationMode = 0
-      setPosition(commandStream, getPosition(commandStream)-1)
+      commandStream.setPosition(getPosition(commandStream)-1)
 
 
   # Execution Mode
   of 0:
     case ch:
     of '0'..'9':
-      var a = float((int(ch) - int('0')))
-      addLast(stack, StackItem(kind: siNumber, numberval: a))
+      stack.addLast(StackItem(kind: siNumber, numberVal: parseFloat($ch)))
       operationMode = -1
+    of '.':
+      stack.addLast(StackItem(kind: siNumber))
+      operationMode = -2
+    of '(':
+      stack.addLast(StackItem(kind: siList))
+      operationMode = 1
+    of 'a'..'z':
+      stack.addLast(register[ch])
+    of 'A'..'Z':
+      register[ch.toLowerAscii()] = stack.popLast()
+    of '+':
+      let b = stack.popLast() # Intentionally b then a, since this is the order for - and /
+      let a = stack.popLast()
+      # FIXME: Check if not a number ...
+      stack.addLast(StackItem(kind: siNumber, numberVal: a.numberVal + b.numberVal))
     else:
       discard
   
   # List Construction Mode
+  # REMARK: Fully implemented
   of 1..high(int):
     case ch:
     of '(':
-      var top = popLast(stack)
+      var top = stack.popLast()
       top.listVal.add('(')
-      addLast(stack, top)
+      stack.addLast(top)
       operationMode += 1
     of ')':
       if operationMode > 1:
-        var top = popLast(stack)
+        var top = stack.popLast()
         top.listVal.add(')')
-        addLast(stack, top)
+        stack.addLast(top)
       operationMode -= 1
     else:
-      var top = popLast(stack)
+      var top = stack.popLast()
       top.listVal.add(ch)
-      addLast(stack, top)
+      stack.addLast(top)
 
+  # Debug stuff
+  echo "Stack:"
+  for i in countdown(len(stack)-1, 0):
+    echo "  ", stack[i]
 
-echo stack
+  echo "\nRegisters:"
+  for i in low(register)..high(register):
+    if register[i] != nil: echo "  ", i, ": ", register[i]
+
+  echo "=".repeat(0x40)
